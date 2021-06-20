8924765a8885532b4a6aaa47eecdc7657dc8dfc2
bhalla <bhalla@ncbs.res.in>
2021-06-20 20:56:26 +0530

    Updates with GPL to htgraph.py

--- htgraph.py
+++ htgraph.py
@@ -1,3 +1,35 @@
+# 
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 3, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+# Floor, Boston, MA 02110-1301, USA.
+# 
+
+'''
+*******************************************************************
+ * File:            htgraph.py
+ * Description:
+ * Author:          G.V. Harsha Rani, Upinder S. Bhalla
+ * E-mail:          hrani@ncbs.res.in, bhalla@ncbs.res.in
+ ********************************************************************/
+
+/**********************************************************************
+** This program converts HILLTAU models defined in JSON format to 
+** reaction diagrams. It draws on the 'dot' program for graphical layout
+** of networks.
+**           copyright (C) 2021 Harsha Rani, Upinder S. Bhalla. and NCBS
+**********************************************************************/
+'''
 '''
 2021
 Mar 31 
